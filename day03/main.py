b681bbe5e8647e5ccc308e28f51917d9f9ade7b8
Jim deVos <jim.devos@gmail.com>
2024-12-03 17:28:33 -0700

    day 3 star two

--- day03/main.py
+++ day03/main.py
@@ -1,6 +1,7 @@
 import re
 
 REX_MUL = re.compile(r'(mul)\((\d{1,3}),(\d{1,3})\)')
+REX_CONDMUL = re.compile(r"(mul)\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don't)\(\)")
 
 def read_lines(path):
   lines = None
@@ -10,17 +11,16 @@ def read_lines(path):
 
 
 
-def read_commands(path):
+def read_commands(path, pattern=REX_MUL):
   lines = read_lines(path)
   allcmds = []
   for line in lines:
-    cmds = REX_MUL.findall(line)
-    print(f'found {len(cmds)} commands: {cmds}')
+    cmds = pattern.findall(line)
+    # print(f'found {len(cmds)} commands')
     #make sure all commands have 3 terms
-    assert(all(len(cmd)==3 for cmd in cmds))
+    # assert(all(len(cmd)==3 for cmd in cmds))
     allcmds += cmds
   return  allcmds
-      
 
 def process_muls(cmds):
   """execute each multiplication, and add the products to a sum"""
@@ -29,19 +29,45 @@ def process_muls(cmds):
   for cmdname, lterm, rterm in cmds:
     lterm = int(lterm)
     rterm = int(rterm)
-    print(cmdname, lterm, rterm, lterm*rterm)
+    # print(cmdname, lterm, rterm, lterm*rterm)
     total += (lterm * rterm)
-  print(f'sum of products: {total}')
+  print(f'star one total: {total}')
 
 
-def main():
+def process_conds(cmds):
+  enabled = True
+  total = 0
+  for cmd in cmds:
+    cmdname, lterm, rterm, isdo, isdont = cmd
+    if cmd[3]:
+      # print("do")
+      enabled = True
+    if cmd[4]:
+      # print("DONT")
+      enabled = False
+    if cmdname and enabled:
+      lterm = int(lterm)
+      rterm = int(rterm)
+      total += lterm * rterm
+  return total
+
+
+
+def main_part1():
   # cmds = read_commands('resources/sample.txt')
   # process_muls(cmds)
   cmds = read_commands('resources/input.txt')
   process_muls(cmds)
 
 
+def main_part2(path):
+  cmds = read_commands(path, REX_CONDMUL)
+  total = process_conds(cmds)
+  print(f'star two total: {total}')
 
+def main():
+  main_part1()
+  main_part2('resources/input.txt')
 
 
 if __name__ == "__main__":
