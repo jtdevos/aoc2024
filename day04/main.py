c31e13bb26b9d7430a3ae4b42078358897caa15a
Jim deVos <jim.devos@gmail.com>
2024-12-04 21:23:30 -0700

    day 4 star 2

--- day04/main.py
+++ day04/main.py
@@ -25,7 +25,7 @@ def read_grid(path):
   return (lines, rows, cols)
 
 
-def find_words_in_grid(word, grid):
+def find_words_in_grid(grid):
   rows = len(grid)
   cols = len(grid[0])
 
@@ -37,7 +37,7 @@ def find_words_in_grid(word, grid):
       ch = '-'
     return ch
 
-  def word_found(r, c, dr, dc):
+  def word_found(word, r, c, dr, dc):
     """
     does the word exist in the grid at the given location in the given direction
     """
@@ -51,25 +51,46 @@ def find_words_in_grid(word, grid):
         return False
       if word[i] != grid[cr][cc]:
         return False
-    print(f'word found at ({r},{c}) in direction ({dr}, {dc})')
+    print(f'{word} found at ({r},{c}) in direction ({dr}, {dc})')
     return True
   
-  findcount = 0
-
-  for direction in DIRECTIONS:  
-    dr, dc = direction
+  def find_xmases():
+    findcount = 0
+    for direction in DIRECTIONS:  
+      dr, dc = direction
+      for i in range(rows):
+        for j in range(cols):
+          if word_found('XMAS', i, j, dr, dc):
+            findcount += 1
+    print(f'total occurrences: {findcount}')
+    return findcount
+  
+  def find_masx():
+    #start at origin (or, oc)
+    #find MAS or SAM at (or, oc) going southeast
+    #find MAS or SAM at (or, oc+2) going southwest
+    findcount = 0
     for i in range(rows):
       for j in range(cols):
-        if word_found(i, j, dr, dc):
+        p1r = i
+        p1c = j
+        p2r = i 
+        p2c = j+2
+        # find MAS or SAM at p1
+        p1found = word_found('MAS', p1r, p1c, 1, 1) or word_found('SAM', p1r, p1c, 1, 1)
+        p2found = word_found('MAS', p2r, p2c, 1, -1) or word_found('SAM', p2r, p2c, 1, -1)
+        masfound = p1found and p2found
+        if masfound:
           findcount += 1
-  
-  print(f'total occurrences: {findcount}')
-  return findcount
+    print(f'x-mases found: {findcount}')
 
+  
+  find_xmases()
+  find_masx()  
 
 def main_part1(path):
   grid, rows, cols = read_grid(path)
-  find_words_in_grid('XMAS', grid)
+  find_words_in_grid(grid)
   
 
 def main_part2(path):
@@ -78,7 +99,7 @@ def main_part2(path):
 
 
 def main():
-  main_part1('resources/sample.txt')
+  # main_part1('resources/sample.txt')
   main_part1('resources/input.txt')
 
   # main_part2('resources/sample.txt')
